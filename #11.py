add3feb8205e12bef646303b1c1dab3290cba281
Ruslan Aniskin <aniskinruslan@gmail.com>
2021-07-25 21:39:27 +0400

    Update #11.py

--- #11.py
+++ #11.py
@@ -1,6 +1,7 @@
 # Найди и замени!
 mylist1 = [2, 5, 11, 19, 27, 31, 40, 53, 65, 77, 80, 99]
 for item in mylist1:
-    a = int(input("Введите число на которое хотите заменить "))
-    mylist1[2]=a
-    print(mylist1)
+    print(item)
+a = int(input("Введите число на которое хотите заменить "))
+mylist1[2] = a
+print(mylist1)
