13f3f5fc4189ef05c256d79fb2a5255360032d8e
Ruslan Aniskin <aniskinruslan@gmail.com>
2021-07-25 12:29:35 +0400

    Update #11.py

--- #11.py
+++ #11.py
@@ -7,7 +7,6 @@
 # "84" из списка заменяется на "3"
 mylist1 = [2, 5, 11, 19, 27, 31, 40, 53, 65, 77, 80, 99]
 for item in mylist1:
-    print(item)
-a = int(input("Введите число на которое хотите заменить "))
-mylist1[2]=a
-print(mylist1)
+    a = int(input("Введите число на которое хотите заменить "))
+    mylist1[2]=a
+    print(mylist1)
